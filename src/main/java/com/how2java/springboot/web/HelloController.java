3014027d53962971703570dded5f5048e7286d0b
MYS1484 <1484826212@qq.com>
2021-05-11 10:11:02 +0800

    项目push

---
+++ src/main/java/com/how2java/springboot/web/HelloController.java
@@ -0,0 +1,14 @@
+package com.how2java.springboot.web;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+ 
+@Controller
+public class HelloController {
+ 
+    @RequestMapping("/hello")
+    public String hello(Model m) {
+    	m.addAttribute("name", "thymeleaf");
+        return "hello";
+    }
+}
